c5be41afe357c24fdce6ebe7c0ca947a0960bd32
Eliaslhl <eliaslahlouh@gmail.com>
2024-11-17 17:10:09 +0100

    add java code

---
+++ src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.example;
+
+public class HelloWorld {
+
+    public String greet(String name) {
+        return "Hello, " + name + "!";
+    }
+
+    public static void main(String[] args) {
+        HelloWorld helloWorld = new HelloWorld();
+
+        if (args.length > 0) {
+            System.out.println(helloWorld.greet(args[0]));
+        } else {
+            System.out.println("No argument provided. Usage: java HelloWorld <name>");
+        }
+    }
+}
---
+++ src/test/java/com/example/HelloWorldTest.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class HelloWorldTest {
+
+    @Test
+    public void testGreetWithArgument() {
+        HelloWorld helloWorld = new HelloWorld();
+        assertEquals("Hello, Jenkins!", helloWorld.greet("Jenkins"));
+    }
+
+    @Test
+    public void testGreetWithAnotherArgument() {
+        HelloWorld helloWorld = new HelloWorld();
+        assertEquals("Hello, ChatGPT!", helloWorld.greet("ChatGPT"));
+    }
+
+    @Test
+    public void testMainWithoutArgument() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        HelloWorld.main(new String[]{});
+        
+        String expectedOutput = "No argument provided. Usage: java HelloWorld <name>" + System.lineSeparator();
+        assertEquals(expectedOutput, outContent.toString());
+        
+        System.setOut(System.out);
+    }
+}
